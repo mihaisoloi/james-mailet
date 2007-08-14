e3fe18a48b54fbcbab1ffcd4bd7e183103077268
Danny Angus <danny@apache.org>
2007-08-14 14:51:19 +0000

    Corrected headers
    git-svn-id: https://svn.apache.org/repos/asf/james/mailet/trunk@565782 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/mailet/MailAddressTest.java
+++ src/test/java/org/apache/mailet/MailAddressTest.java
@@ -1,13 +1,21 @@
-/*
- * Created on 14 Aug 2007
- *
- * PVCS Workfile Details:
- * $Workfile$
- * $Revision$
- * $Author$
- * $Date$
- * $Modtime$
- */
+/****************************************************************
+ * Licensed to the Apache Software Foundation (ASF) under one   *
+ * or more contributor license agreements.  See the NOTICE file *
+ * distributed with this work for additional information        *
+ * regarding copyright ownership.  The ASF licenses this file   *
+ * to you under the Apache License, Version 2.0 (the            *
+ * "License"); you may not use this file except in compliance   *
+ * with the License.  You may obtain a copy of the License at   *
+ *                                                              *
+ *   http://www.apache.org/licenses/LICENSE-2.0                 *
+ *                                                              *
+ * Unless required by applicable law or agreed to in writing,   *
+ * software distributed under the License is distributed on an  *
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY       *
+ * KIND, either express or implied.  See the License for the    *
+ * specific language governing permissions and limitations      *
+ * under the License.                                           *
+ ****************************************************************/
 
 package org.apache.mailet;
 
@@ -16,10 +24,9 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.ParseException;
 import junit.framework.TestCase;
 
+
 /**
- * @author angusd 
- * @author $Author$ 
- * @version $Revision$
+ * Tests for Test method for {@link org.apache.mailet.MailAddress}.
  */
 public class MailAddressTest extends TestCase {
     private static final String GOOD_LOCAL_PART = "\"quoted@local part\"";
@@ -213,9 +220,3 @@ public class MailAddressTest extends TestCase {
         assertTrue(a.toString()+" != "+b.toString(),a.equals(b));
     }
 }
-/* 
- *
- * PVCS Log History:
- * $Log$
- *
- */
